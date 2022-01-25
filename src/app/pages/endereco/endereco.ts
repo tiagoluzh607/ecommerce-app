0ad0702c9886102d45627ba65156229bd09183f6
Tiago Luz <tiagoluz.h607@gmail.com>
2022-01-25 16:16:33 -0300

    Aula 17 ok

--- src/app/pages/endereco/endereco.ts
+++ src/app/pages/endereco/endereco.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
   })
   export class EnderecoPage {
 
+    endereco: string = 'R. Guia Lopes, 4201 - Canudos, Novo Hamburgo';
+
+    verMapa(){
+      let url = 'https://www.google.com/maps/search/?api=1&query='+this.endereco;
+      this.abrirUrl(url);
+    }
+
+    ligar(){
+     let url = 'tel:5551988888888'
+     this.abrirUrl(url);
+    }
+
+    verWhatsapp(){
+      let url = 'https://api.whatsapp.com/send?phone=5551988888888&text=Ola'  
+      this.abrirUrl(url);
+    }
+
+    private abrirUrl(url: string){
+      window.open(url, '_blank').focus();
+    }
+
   }
