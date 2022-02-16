c246aabe6e0d475da1227380dc0a2f457be26f60
Tiago Luz <tiagoluz.h607@gmail.com>
2022-02-16 12:22:31 -0300

    aula 26 ok

--- src/app/components/card-produto/card-produto.component.ts
+++ src/app/components/card-produto/card-produto.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Produto } from '../../dominio/produto/produto.model';
 
 
 @Component({
@@ -7,5 +8,16 @@ import { Component } from '@angular/core';
     styleUrls: ['card-produto.component.scss'],
 })
 export class CardProdutoComponent {
-    
+    @Input() produto: Produto;
+    avaliacoes: boolean[] = [true,true,false,false,false]
+
+    ngOnInit(){
+        this.preencheAvaliacoes(this.produto.avaliacao);
+    }
+
+    preencheAvaliacoes(avaliacao: number){
+        // avaliacao 3 = [true,true,true,false,false]
+        //avaliacao 4 = [true,true,true,true,false]
+        this.avaliacoes = this.avaliacoes.map((a,i)=> i+1 <= avaliacao);
+    }
 }
