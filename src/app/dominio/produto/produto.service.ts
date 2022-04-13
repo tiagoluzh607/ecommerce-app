97d04462bc184dac691e3e2fa60c6cc17512f5fb
tiagoh607 <tiagoluz.h607@gmail.com>
2022-04-13 16:16:18 -0300

    Aula 08 - URL da API em enviroment

--- src/app/dominio/produto/produto.service.ts
+++ src/app/dominio/produto/produto.service.ts
@@ -1,12 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 import { Produto } from './produto.model';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ProdutoService{
+
+    API = environment.urlApi;
+    endpoint = 'produtos';
     
     constructor(
         private http: HttpClient
@@ -33,7 +37,7 @@ export class ProdutoService{
     }
 
     selectAllAPI(): Observable<Produto[]>{
-        return this.http.get<Produto[]>('http://localhost:3000/produtos');
+        return this.http.get<Produto[]>(`${this.API}/${this.endpoint}`);
     }
 
     selectId(id: number): Produto {
