7df94cba70dcb87b9dc30929c37aff95b8ea6984
tiagoh607 <tiagoluz.h607@gmail.com>
2022-06-22 12:05:33 -0300

    AAula 24 - Criando uma CarrinhoDAO com LocalStorage

---
+++ src/app/dominio/carrinho/carrinho-dao.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { Produto } from "../produto/produto.model";
+
+const KEY = 'carrinho'
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CarrinhoDaoService{
+
+    set(produtos: Produto[]){
+        window.localStorage.setItem(KEY, JSON.stringify(produtos));
+    }
+
+    get(): Produto[]{
+        let objectString =  window.localStorage.getItem(KEY);
+        return JSON.parse(objectString);
+    }
+
+    remove(){
+        window.localStorage.removeItem(KEY);
+    }
+
+    has(): boolean{
+       return !!this.get();
+    }
+
+}
